b4757755c1016e980d5d05b04782cb87d7ec9510
agungardynto <agungardynto@gmail.com>
2020-07-03 17:54:31 +0700

    add client

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,24 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use App\Client;
+use App\Home;
 
 class PagesController extends Controller
 {
 
     public function home(){
-        return view('user.content.home');
+        $home = Home::all();
+        $client = Client::all();
+        return view('user.content.home', compact('home', 'client'));
     }
 
     public function product(){
-        return view('user.content.product');
+        $home = Home::all();
+        return view('user.content.product', compact('home'));
     }
 
     public function client(){
+        $home = Home::all();
         $clients = Client::all();
-        return view('user.content.client', compact('clients'));
+        return view('user.content.client', compact('home', 'clients'));
     }
 
     public function contact(){
-        return view('user.content.contact');
+        $home = Home::all();
+        return view('user.content.contact', compact('home'));
     }
 }
