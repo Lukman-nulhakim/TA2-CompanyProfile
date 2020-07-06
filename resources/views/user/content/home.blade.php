49b00dbc454b8b87cb70ecd67a11b726d79957b4
unknown <lukmannurhakim1402@gmail.com>
2020-07-06 10:11:33 +0700

    udah siap ke master

--- resources/views/user/content/home.blade.php
+++ resources/views/user/content/home.blade.php
@@ -32,6 +32,49 @@
 
 
 {{-- PRODUK --}}
+<div class="container my-5">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="section-tittle mb-55">
+                <div class="front-text">
+                    <h2 class="mt-5">Our Product</h2>
+                </div>
+                <span class="back-text">Product</span>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <!-- Nav Card -->
+            <div class="tab-content active" id="nav-tabContent">
+                <div class="tab-pane fade active show" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">           
+                    <div class="project-caption">
+                        <div class="row">
+                            @forelse ($product as $item)
+                            <div class="col-lg-4 col-md-6">
+                                <div class="single-project mb-30">
+                                    <div class="project-img">
+                                        <img src="{{ Storage::url($item->image) }}" alt="">
+                                    </div>
+                                    <div class="project-cap">
+                                        <h4 class="my-3">{{ $item->nama }}</h4>
+                                        <p>{{ $item->description }}</p>
+                                        <h4>{{ $item->detail_product->detail_id }}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                            @empty
+                                <h1>Belum ada prouduct</h1>
+                            @endforelse
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 {{-- END PRODUK --}}
 
 
